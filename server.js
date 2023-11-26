9bfa8c6ad9eedf765b67ae8fd0c38ca280dd0d2e
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2023-11-26 14:03:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1723,7 +1723,7 @@ app.post('/quiz', (req, res) => { // post because get won't work with Canvas
 	passed = lmsData.body.custom_passed;
 	}
 	else {
-		sessions = 'request not valid: ' + err;
+		sessions = 'request not valid: ' + err + ' req: ' + JSON.stringify(req) + ' lmsData: ' + JSON.stringify(lmsData);
 		console.log('invalid request')
 		passed = req.body.custom_passed;
 	}
